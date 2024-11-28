b51a91dd559e88790b9d17ed8b6012604b9d25e6
Monmoonluna <minhnguyetdn2004@gmail.com>
2024-11-28 14:47:34 +0700

    Chapter1/Homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,24 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String name, surname;
+            int age;
+            float height, weight;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter your surname: ");
+            surname = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your height(cm): ");
+            height = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Enter your weight(kg): ");
+            weight = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, " + "his weight is " + weight + " kg." + " and his height is " + height + " cm.");
+            height /= 100;
+            float bmi = weight / (height * height);
+            Console.WriteLine("BMI: " + bmi);
+
         }
     }
 }
